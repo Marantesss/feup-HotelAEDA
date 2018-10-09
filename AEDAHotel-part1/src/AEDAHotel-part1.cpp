00367a6a220c349954641b0a4664c820ef1fe669
Marantesss <up201706917@fe.up.pt>
2018-10-09 19:34:32 +0100

    first commit

---
+++ AEDAHotel-part1/src/AEDAHotel-part1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AEDAHotel-part1.cpp
+// Author      : Goncalo Marantes, Joao Santos, Luis Goncalves
+// Version     : 1.0
+// Copyright   : MIT License
+// Description : AEDAHotel
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Estamos todos chumbados! :´)" << endl; // prints !!!Hello World!!!
+	return 0;
+}
