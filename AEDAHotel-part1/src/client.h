6fbfa980f04189c5e19c4b84355954b46b139efc
Gonçalo Marantes <up201706917@fe.up.pt>
2018-11-21 18:08:42 +0000

    Finished client.h doxygen documentation

--- AEDAHotel-part1/src/client.h
+++ AEDAHotel-part1/src/client.h
@@ -10,89 +10,118 @@
 using namespace std;
 
 /**
-*  A class used to represent a client.
-*  Each client object has a name and a reservations vector (further explanation on the Reservation Class).
-*/
+ *  A class used to represent a client.
+ *  Each client object has a name and a reservations vector
+ *  @see Reservation Class
+ */
 class Client {
-	string name;	/**< string name. The client's name. */
-	vector<Reservation*> reservations;	/**< vector<Reservation> reservations. The client's hotel reservations. */
+	/**
+	 * string name The client's name
+	 */
+	string name;
+
+	/**
+	 * vector<Reservation*> reservations The client's hotel reservations.
+	 */
+	vector<Reservation*> reservations;
 public:
 	/**
-	* A default constructor.
-	*/
+	 * @brief Default constructor.
+	 */
 	Client();
 
 	/**
-	* A constructor.
-	* The constructor creates a Client object with the data passed as argument.
-	* @param name The client's name.
-	*/
+	 * @brief Constructor.
+	 *
+	 * The constructor creates a Client object with the data passed as argument.
+	 *
+	 * @param name The client's name.
+	 */
 	Client(string name);
 
 	/**
-	* A default destructor.
-	*/
+	 * @brief Default destructor.
+	 */
 	~Client();
 
 	/**
-	* A const member function with no arguments to get the client's name.
-	* @return A string, the client's name.
-	*/
+	 * @brief Const member function to get the client's name.
+	 *
+	 * @return The client's name.
+	 */
 	string getName() const;
 
 	/**
-	* A member function with no return value to change the client's name.
-	* @param name The client's new name.
-	*/
+	 * @brief Member function to change the client's name.
+	 *
+	 * @param name The client's new name.
+	 */
 	void setName(string name);
 
 	/**
-	* A const member function with no arguments to get the client's information.
-	* @return A string, the client's name and reservations.
-	*/
+	 * @brief Const member function to get the client's information.
+	 *
+	 * The return string will contain the client's name
+	 * followed by newline and a list containing all the reservations made
+	 *
+	 * @return The client's name and reservations.
+	 */
 	string getInfo() const;
 
 	/**
-	* A member function with no return value to add a new reservation.
-	* @param R The client's new reservation.
-	*/
+	 * @brief Member function to add a new reservation.
+	 *
+	 * All the reservations are pointers to the reservations vector
+	 * in the hotel class
+	 *
+	 * @param R Pointer to client's new reservation.
+	 */
 	void addReservation(Reservation * R);
 
 	/**
-	* Overload of Operator < for class Client.
-	* @param c2 The other client to be compared to.
-	* @return true, if the first client's name comes first in alphabetical order
-	* 		  false, if otherwise
-	*/
+	 * @brief Overload of Operator < for class Client.
+	 *
+	 * @param c2 The other client to be compared to.
+	 *
+	 * @return true, if the first client's name comes first in alphabetical order
+	 * 	false, if otherwise
+	 */
 	bool operator < (Client & c2);
 
 	/**
-	* Overload of Operator == for class Client.
-	* @param c2 The other client to be compared to.
-	* @return true, if both the clients' name is the same
-	* 		  false, if otherwise
-	*/
+	 * @brief Overload of Operator == for class Client.
+	 *
+	 * @param c2 The other client to be compared to.
+	 *
+	 * @return true, if both the clients' name is the same
+	 * 		  false, if otherwise
+	 */
 	bool operator == (Client & c2);
 };
 
 /**
-*  A class used to represent an exception.
-*  The exception object contains the invalid client.
-*/
+ *  A class used to represent an exception.
+ *  The exception object contains the invalid client.
+ */
 class NonExistingClient {
-	string name;	/**< string name. The NonExistingClient's name. */
+	/**
+	 * string name The NonExistingClient's name.
+	 */
+	string name;
 public:
 	/**
-	* A constructor.
-	* The constructor creates a NonExistingClient object with the supplied name.
-	* @param A string, the name of the NonExistingClient.
-	*/
+	 * @brief Constructor.
+	 *
+	 * The constructor creates a NonExistingClient object with the supplied name.
+	 *
+	 * @param The name of the NonExistingClient.
+	 */
 	NonExistingClient(string nm);
 
 	/**
-	* A const member function with no arguments to get the object's name.
-	* @return A string, the name that originated the creation of this object.
-	*/
+	 * @brief Const member function with no arguments to get the object's name.
+	 * @return The name that originated the creation of this object.
+	 */
 	string getName() const;
 };
 
